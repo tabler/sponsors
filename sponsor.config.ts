300bad9246aaecc93d52087479964c4187e7224a
codecalm <codecalm@gmail.com>
2025-01-14 17:28:12 +0100

    feat: initialize sponsorship configuration and workflows

---
+++ sponsor.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, tierPresets } from 'sponsorkit';
+
+export default defineConfig({
+  renderer: 'tiers',
+  formats: ['svg', 'png'],
+  tiers: [
+    // Past sponsors, currently only supports GitHub
+    {
+      title: 'Past Sponsors',
+      monthlyDollars: -1,
+      preset: tierPresets.xs,
+    },
+    // Default tier
+    {
+      title: 'Backers',
+      preset: tierPresets.base,
+    },
+    {
+      title: 'Sponsors',
+      monthlyDollars: 10,
+      preset: tierPresets.medium,
+    },
+    {
+      title: 'Silver Sponsors',
+      monthlyDollars: 50,
+      preset: tierPresets.large,
+    },
+    {
+      title: 'Gold Sponsors',
+      monthlyDollars: 100,
+      preset: tierPresets.xl,
+    },
+  ],
+});
